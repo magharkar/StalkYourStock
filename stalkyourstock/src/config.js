7970075e3d07914d080e4c5904d452544e839028
rshinde <rc382783@dal.ca>
2022-04-05 20:09:15 -0300

    Update config and Dockerfile

--- stalkyourstock/src/config.js
+++ stalkyourstock/src/config.js
@@ -1,3 +1,5 @@
-const baseURL = "http://localhost:5000";
+//const baseURL = "http://localhost:5000";
+
+const baseURL = window.location.protocol + "//" + window.location.hostname +":5000";
 
 export default baseURL;
