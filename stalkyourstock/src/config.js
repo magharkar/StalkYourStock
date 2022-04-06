93b0eee7da8df7f5151599d15744684b035a16fc
Nikita Kothari <nk912794@dal.ca>
2022-04-06 18:01:09 -0300

    testing application deployment

--- stalkyourstock/src/config.js
+++ stalkyourstock/src/config.js
@@ -1,5 +1,5 @@
 //const baseURL = "http://localhost:5000";
-
-const baseURL = window.location.protocol + "//" + window.location.hostname +":5000";
+const baseURL = "http://Sysbackendapp-env.eba-ktpmucfp.us-east-1.elasticbeanstalk.com";
+//const baseURL = window.location.protocol + "//" + window.location.hostname +":5000";
 
 export default baseURL;
